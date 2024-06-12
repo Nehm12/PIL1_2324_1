6c8e73214f3aede5e8b5449a81847f51cedb6967
Nehm12 <houngajunior72@gmail.com>
2024-06-12 11:06:03 +0100

    oklm

--- Rencontre/Rencontre/settings.py
+++ Rencontre/Rencontre/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Account',
+    'Chat',
+    'Notification',
+    'Profils',
+    'Suggest',
 ]
 
 MIDDLEWARE = [
